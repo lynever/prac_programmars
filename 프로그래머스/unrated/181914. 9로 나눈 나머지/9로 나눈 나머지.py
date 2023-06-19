489e0ceacb2eef422ae0e756fe855d9590a70459
Choi Soo Lyn <31681740+lynever@users.noreply.github.com>
2023-06-20 08:39:40 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.py
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.py
@@ -1,6 +1,2 @@
 def solution(number):
-    answer = 0
-    tmplst = list(str(number))
-    for i in tmplst:
-        answer += int(i)
-    return answer % 9
+    return sum(list(map(int, number))) % 9
