902a22900b0ff0ed41351ae33cfdc020077863f1
Choi Soo Lyn <31681740+lynever@users.noreply.github.com>
2023-07-04 10:07:16 +0900

    [unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,8 @@
+def solution(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return 1 if len(arr1) > len(arr2) else -1
+    else:
+        if sum(arr1) == sum(arr2):
+            return 0
+        else:
+            return 1 if sum(arr1) > sum(arr2) else -1
