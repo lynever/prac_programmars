44b5e9f2be325eddb8ecd7ce6b3e6c918dea8324
Choi Soo Lyn <31681740+lynever@users.noreply.github.com>
2023-07-04 10:10:13 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -0,0 +1,3 @@
+def solution(arr, n):
+    return [val + n if idx % 2 == 0 else val for idx, val in enumerate(arr)] if len(arr) % 2 == 1 else [val + n if idx % 2 == 1 else val for idx, val in enumerate(arr)]
+    return answer
